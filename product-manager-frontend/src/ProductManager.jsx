2e3324c93857da718942ab6374b970ecf7d74d3b
SwastikKaurav <kauravswastik15@gmail.com>
2026-06-16 22:31:20 +0530

    Heading Tag Added

--- product-manager-frontend/src/ProductManager.jsx
+++ product-manager-frontend/src/ProductManager.jsx
@@ -2,21 +2,33 @@ import {useEffect, useState} from "react"
 
 export default function ProductManager(){
     let [products, setProducts] = useState([])
+    const [form, setForm] = useState({name:"",description:"",price:"",quantity:""})
 
-    useEffect(()=>{
-        async function loadProducts(){
-        const res = await fetch("http://localhost:8000/products/")
-        const data = await res.json()
-        setProducts(data)
+    useEffect(() => {
+    async function loadProducts() {
+        try {
+            const res = await fetch("http://localhost:8000/products/");
+            const data = await res.json();
+            setProducts(data);
+        } catch (error) {
+            console.log(error);
         }
-        loadProducts();
-    },[])
+    }
+
+    loadProducts();
+}, []);
 
     return(
-        <>
-        {products.map((product)=>(
-            <p key={product.id}>{product.name} - {product.description}</p>
-        ))}
-        </>
-    )
+    <>
+    <h1 className="heading">Product Manager</h1>
+    <p>Connected to FastAPI at http://localhost:8000</p>
+    {products.map((product) => (
+        <div key={product.id}>
+            <p>{product.name} - {product.description}</p>
+            <p>${product.price} | qty: {product.quantity}</p>
+        </div>
+    ))}
+    
+    </>
+)
 }
